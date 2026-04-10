aacc492d74ee26d2a72dcd898acf899d496bce73
Navab Jhon shaik <nawabjanshaik86@gmail.com>
2026-04-10 15:19:43 +0530

    Delete modules directory

--- modules/alb/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "vpc_id" {
-  description = "VPC ID where ALB will be created"
-  type        = string
-}
-
-variable "public_subnets" {
-  description = "List of public subnet IDs for ALB"
-  type        = list(string)
-}
